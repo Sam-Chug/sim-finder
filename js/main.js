4d63b6245d055da5da5aaeb153ef71dc99423232
Sam-Chug <sfackler96@gmail.com>
2023-04-11 20:36:35 -0400

    lots online + css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 const SIM_ONLINE_URL = "https://api.freeso.org/userapi/avatars/online";
+const LOTS_ONLINE_URL = "https://api.freeso.org/userapi/city/1/lots/online";
 var LONG_SIM_URL = "";
+var LONG_LOTS_URL = "";
 
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener("DOMContentLoaded", getOnline);
 
-async function ready(){
+// main
+async function getOnline(){
 
-    const simObjList = await grabAPI(SIM_ONLINE_URL);
-
-    LONG_SIM_URL = buildSimLink(simObjList);
+    // Grab sims
+    const simShort = await grabAPI(SIM_ONLINE_URL);
+    LONG_SIM_URL = buildSimLink(simShort);
     const simLong = await grabAPI(LONG_SIM_URL);
 
-    createTable(simLong);
-    simsOnlineCount(simLong);
+    // Grab lots
+    const lotShort = await grabAPI(LOTS_ONLINE_URL);
+    LONG_LOTS_URL = buildLotLink(lotShort);
+    const lotLong = await grabAPI(LONG_LOTS_URL);
+    
+    // Build sims
+    createSimsTable(simLong);
+    writeToLabel("Sims Online: ", simShort.avatars_online_count, "sims-online-count-label");
+
+    // Build lots
+    lotShort.lots.sort(({avatars_in_lot:a}, {avatars_in_lot:b}) => b - a);
+    createLotsTable(lotShort);
+    writeToLabel("Lots Online: ", lotShort.total_lots_online, "lots-online-count-label");
 }
 
+// Return json objects from api links
 async function grabAPI(apiLink){
 
     let obj;
@@ -25,6 +40,7 @@ async function grabAPI(apiLink){
     return obj;
 }
 
+// Builds api link from online sim's avatar_id
 function buildSimLink(simList){
 
     var simIdString = "https://api.freeso.org/userapi/avatars?ids=";
@@ -37,34 +53,42 @@ function buildSimLink(simList){
     return simIdString;
 }
 
+// Builds api link from online lot's lot_id
+function buildLotLink(lotList){
+
+    var lotIDString = "https://api.freeso.org/userapi/lots?ids=";
+    for(i = 0; i < lotList.lots.length; i++){
+
+        lotIDString += lotList.lots[i].lot_id + ",";
+    }
+
+    lotIDString = lotIDString.slice(0, -1);
+    return lotIDString;
+}
+
+// Return sim's age in days
 function simDayAge(unixAge){
 
     const now = Math.round(Date.now() / 1000);
-
     return Math.round((now - unixAge) / 86400);
 }
 
-function createTable(simList){
+// Write to online sims table
+function createSimsTable(simList){
 
-    const target = document.getElementById('target');
+    const target = document.getElementById('sims-table');
 
     const tableHead = document.createElement("thead");
     const headRow = document.createElement("tr");
-
     const thName = document.createElement("th");
-    const thID = document.createElement("th");
     const thAge = document.createElement("th");
 
     thName.textContent = "Name";
-    thID.textContent = "ID";
     thAge.textContent = "Age";
-
     headRow.appendChild(thName);
-    headRow.appendChild(thID);
     headRow.appendChild(thAge);
 
     tableHead.appendChild(headRow);
-
     target.appendChild(tableHead);
 
     const tableBody = document.createElement("tbody");
@@ -72,27 +96,64 @@ function createTable(simList){
 
         const newRow = document.createElement("tr");
         const tdName = document.createElement("td");
-        const tdID = document.createElement("td");
         const tdAge = document.createElement("td");
 
         tdName.textContent = simList.avatars[i].name;
-        tdID.textContent = simList.avatars[i].avatar_id;
         tdAge.textContent = simDayAge(simList.avatars[i].date) + " days";
 
         newRow.appendChild(tdName);
-        newRow.appendChild(tdID);
         newRow.appendChild(tdAge);
 
         tableBody.appendChild(newRow);
     }
-
     target.appendChild(tableBody);
 }
 
-function simsOnlineCount(simList){
+// Write to online sims table
+function createLotsTable(lotList){
 
-    const simCount = "There are " + simList.avatars.length + " sims online:"
-    const location = document.getElementById("p");
-    
-    location.textContent = simCount;
+    const target = document.getElementById('lots-table');
+
+    const tableHead = document.createElement("thead");
+    const headRow = document.createElement("tr");
+
+    const thName = document.createElement("th");
+    const thPop = document.createElement("th");
+    thName.textContent = "Name";
+    thPop.textContent = "Population";
+
+    headRow.appendChild(thName);
+    headRow.appendChild(thPop);
+
+    tableHead.appendChild(headRow);
+    target.appendChild(tableHead);
+
+    const tableBody = document.createElement("tbody");
+    for(i = 0; i < lotList.lots.length; i++){
+
+        const newRow = document.createElement("tr");
+        const tdName = document.createElement("td");
+        const tdPop = document.createElement("td");
+
+        tdName.textContent = lotList.lots[i].name;
+        tdPop.textContent = lotList.lots[i].avatars_in_lot + " sims";
+
+        newRow.appendChild(tdName);
+        newRow.appendChild(tdPop);
+
+        tableBody.appendChild(newRow);
+    }
+    target.appendChild(tableBody);
+}
+
+// Write amount of sims online
+function writeToLabel(contentString, content, target){
+
+    const location = document.getElementById(target);
+    const labelText = contentString + content;
+
+    console.log(labelText);
+    console.log(location);
+
+    location.textContent = labelText;
 }
