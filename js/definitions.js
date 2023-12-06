6d21e52fa7f5c483227993d0783848d15602e253
Sam-Chug <sfackler96@gmail.com>
2023-12-05 21:10:31 -0500

    - cache bust to 0.2.2d

--- js/definitions.js
+++ js/definitions.js
@@ -1,5 +1,5 @@
 //#region Strings
-const VERSION_STR = "v0.2.2c";
+const VERSION_STR = "v0.2.2d";
 
 const SIM_ONLINE_URL = "https://api.freeso.org/userapi/avatars/online";
 const LOTS_ONLINE_URL = "https://api.freeso.org/userapi/city/1/lots/online";
@@ -322,12 +322,12 @@ const GUI_COLORMODE_BUTTON = document.getElementById('colormode-button');
 const CUSTOM_STYLE_REAGAN = "Reaganomics Lamborghini";
 
 const CUSTOM_STYLE_SIMHEADS = {
-    male: "./images/sim-faces/simface-m.png?v0.2.2c",
-    female: "./images/sim-faces/simface-f.png?v0.2.2c",
-    reagan: "./images/sim-faces/simface-rea.png?v0.2.2c",
-    bear: "./images/sim-faces/simface-b.png?v0.2.2c"
+    male: "./images/sim-faces/simface-m.png?v0.2.2d",
+    female: "./images/sim-faces/simface-f.png?v0.2.2d",
+    reagan: "./images/sim-faces/simface-rea.png?v0.2.2d",
+    bear: "./images/sim-faces/simface-b.png?v0.2.2d"
 };
-const IMAGE_UNKNOWN_SIM = new Image().src = "./images/sim-faces/simface-u.png?v0.2.2c"
+const IMAGE_UNKNOWN_SIM = new Image().src = "./images/sim-faces/simface-u.png?v0.2.2d"
 
 const CUSTOM_STYLE_BLOCK = {
     bp: {
